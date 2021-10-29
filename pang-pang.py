baaa67c34d72d253f4714980c49b22620780e9c9
William Salenius <william.salenius@skola.taby.se>
2021-10-29 11:12:46 +0200

    file update

--- pang-pang.py
+++ pang-pang.py
@@ -1,22 +1,54 @@
+from random import randint
+
 class Player():
     def __init__(self, lifes):
-        self.lifes = self.lifes
+        self.lifes = lifes
         self.scores = 0
         self.did_hit = False
         self.is_hitted = False
 
     def fire(self):
-        pass
+        scope = randint(1, 5)
+        if scope in {1, 2, 3}:
+            self.did_hit = True
+            self.is_hittted = False
+        else:
+            self.did_hit = False
+            self.is_hittted = True
        # Här sker "eldväxlingen"
 
     def inc_scores(self):
-        pass
+        if self.did_hit == True:
+            self.scores += 1
+        
        # Här ska poängen öka
 
     def reduce_lifes(self):
-        pass
+        if self.is_hitted == True:
+            self.lifes -= 1
+        
         # Här ska antalet liv minska
 
-
 a_player = Player(3)       # Initiera en spelare med tre liv
-a_player.fire()            # Spelaren skjuter
+
+
+while True:
+    input('Tryck Enter för att skjuta')
+    a_player.fire()
+    if a_player.did_hit:
+        print('Träff!')
+        a_player.inc_scores() # Antalet poäng ökas med 1
+    else:
+        print('Miss, sikta bättre')
+    if a_player.is_hitted == True:
+        print('Aaaaaah')
+        a_player.reduce_lifes() # Antalet liv minskas med 1
+        print(f"Dina liv {a_player.lifes}")
+        print(f"Dina poäng {a_player.scores}")
+    else:
+        a_player.inc_scores()
+        print('Du klarade dig denna gång!')
+        print(f"Dina poäng: {a_player.scores}")
+        print(f"Dina liv: {a_player.lifes}")
+    if a_player.lifes <= 0:
+        break
